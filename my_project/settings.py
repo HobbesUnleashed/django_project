159f0843c940d074df86362743a2b2f0ec7ec17b
Shay <sleacy@googlemail.com>
2025-04-06 10:29:59 +0100

    Settings.py deep dive

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-dhkre=oe54zoss(d-(+$myiz^bcf-q+nvcbl)-*8gs2(&9xhr$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     ".herokuapp.com",
